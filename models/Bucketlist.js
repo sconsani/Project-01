60ef18906b742e48149e69dc445cdd4188815c8e
lsack <lauragsack@gmail.com>
2020-03-24 16:41:31 -0700

    commits server.js updates and routes/controllers directories

--- models/Bucketlist.js
+++ models/Bucketlist.js
@@ -3,8 +3,8 @@ const DonutStore = require("./DonutStore");
 
 const BucketlistSchema = new mongoose.Schema ({
     bucketlist: [DonutStore.Schema],
-    visited: Boolean,
-    visitedStores: [DonutStore.Schema],
+    // visited: Boolean, // if the entire bucketlist was visited
+    visitedStores: [DonutStore.Schema], // if store id exists in list, it was visited
     comments: String,
     rating: Number,
 });
