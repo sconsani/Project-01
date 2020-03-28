82afdc413a7c50f0441b51ae71a5b93a2dbda3c5
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2020-03-27 18:30:55 -0700

    local changes to show js

--- public/scripts/bucketlistShow.js
+++ public/scripts/bucketlistShow.js
@@ -34,8 +34,7 @@ function renderBucketlist(bucketlist) {
 function donutCard(donutStore) {
 
 return `<div class = "card mb-3" style= "min-width:15rem; max-width:15rem;">
-<button type="button" class="close" aria-label="Close">
-  <span aria-hidden="true">&times;</span>
+<button type="button" class="close" id=${donutStore._id} aria-label="Close">x
 </button>
 <img class = "card-img-top" src = "${donutStore.photo.photo_reference}" alt = "donut image"
 style = "width: 100%; height: 15vw; object-fit: cover;">
@@ -46,10 +45,9 @@ style = "width: 100%; height: 15vw; object-fit: cover;">
     <li class = "list-group-item">${donutStore.rating}</li>
     <li class = "list-group-item">${donutStore.weekday_text}</li>
 </ul>
-	<div class = "card-body">
-	<button type="button" class="add btn btn-primary btn-sm btn-success position-absolute mid-center" id=${donutStore._id}>DOUGHNIT</button>
-	<button type="button" class="add btn btn-secondary btn-sm btn-outline-danger position-absolute mid-center" id=${donutStore._id}>Remove</button>
-	</div>
+    <div class = "card-body">
+    <button type="button" class="add btn btn-primary btn-sm btn-success position-absolute mid-center btn-block" id=${donutStore._id}>DOUGHNIT</button>
+    </div>
 </div>
 </div>
     `;
